260ecd48139bddc29ccf25949a6a86dc81d00bf8
Aurelioss <baaurelienba@gmail.com>
2022-01-19 16:20:51 +0100

    added style

--- index.php
+++ index.php
@@ -9,46 +9,43 @@
     <?php include 'includes/navbar.php'; ?>
     <br><br>
 
-    <div class="container">
-    
+    <div class="page-wrapper bg-gra-02 p-t-130 p-b-100 font-poppins">
+        <label class="nav-title m-l-45">Forum</label>
         <form method="GET">
-
-            <div class="form-group row">
-
-                <div class="col-8">
-                    <input type="search" name="search" class="form-control">
+            <div class="row p-t-30 m-l-45">
+                <div>
+                    <input type="search" name="search" class="input--style-4" placeholder="Type what you want">
                 </div>
-                <div class="col-4">
-                    <button class="btn btn-success" type="submit">Rechercher</button>
+                <div class="m-l-45">
+                    <button class="btn btn--radius-2 btn--blue" type="submit">Rechercher</button>
                 </div>
-
             </div>
         </form>
-
         <br>
-
-        <?php 
-            while($question = $getAllQuestions->fetch()){
-                ?>
-                <div class="card">
-                    <div class="card-header">
-                        <a href="article.php?id=<?= $question['id']; ?>">
-                            <?= $question['title']; ?>
-                        </a>
-                    </div>
-                    <div class="card-body">
+        <div class="wrapper wrapper--w960 p-t-20">
+            <?php 
+                while($question = $getAllQuestions->fetch()){
+            ?>
+            <div class="card card-4">
+                <div class="card-body">
+                    <a href="article.php?id=<?= $question['id']; ?>" class="title">
+                        <?= $question['title']; ?>
+                        question titre
+                    </a>
+                    <div class="p-t-30">
                         <?= $question['description']; ?>
+                        description
                     </div>
-                    <div class="card-footer">
-                        Publié par <a href="profile.php?id=<?= $question['id_auteur']; ?>"><?= $question['pseudo_auteur']; ?></a> le <?= $question['date_publication']; ?>
-                    </div>
+                        <div class="p-t-20">
+                            Publish by <a href="profile.php?id=<?= $question['id_auteur']; ?>"><?= $question['pseudo_auteur']; ?></a> le <?= $question['date_publication']; ?>
+                        </div>
                 </div>
-                <br>
-                <?php
-            }
-        ?>
-
+            </div>
+            <br>
+            <?php
+                }
+            ?>
+        </div>
     </div>
-
 </body>
 </html>
