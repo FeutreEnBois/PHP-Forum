5d40d29453f6aa70a1f984fadff3ccbfee922322
Aurelioss <baaurelienba@gmail.com>
2022-01-19 14:42:46 +0100

    added style

--- edit-question.php
+++ edit-question.php
@@ -14,7 +14,7 @@ require('actions/questions/editQuestionModels.php');
     <?php require('includes/navbar.php'); ?>
 
     <br><br>
-    <div class="container">
+    <div class="page-wrapper bg-gra-02 p-t-130 p-b-100 font-poppins">
         <?php
         if (isset($errorMsg)) {
             echo '<p>' . $errorMsg . '</p>';
@@ -24,22 +24,34 @@ require('actions/questions/editQuestionModels.php');
         <?php
         if (isset($question_content)) {
         ?>
-            <form class="container" method="POST">
 
+        <label class="nav-title m-l-45">Edit message</label>
 
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Post Title</label>
-                    <input type="text" class="form-control" name="title" placeholder="Enter here your username" value="<?= $question_title ?>">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Post Description</label>
-                    <textarea class="form-control" name="description" placeholder="Enter here your lastname"><?= $question_description ?></textarea>
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Post Content</label>
-                    <textarea class="form-control" name="content" placeholder="Enter here your firstname"><?= $question_content ?></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary" name="validate">Modify Post</button>
+            <form method="POST">
+
+            <div class="wrapper wrapper--75p p-t-20">
+                <div class="card card-4">
+                    <div class="card-body">
+
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="">Edit Title :</label> <br>
+                <input type="text" class="input--style-4" name="title" placeholder="Enter the new title">
+            </div>
+            <div class="p-t-15">
+                <label for="exampleInputPassword1" class="">Edit Description :</label> <br>
+                <textarea class="txtareas" name="description" placeholder="Enter here your lastname"><?= $question_description ?></textarea>
+            </div>
+            <div class="p-t-15">
+                <label for="exampleInputPassword1" class="form-label">Edit Content :</label> <br>
+                <textarea class="txtareas" name="content" placeholder="Enter here your firstname"><?= $question_content ?></textarea>
+            </div>
+            <div class="p-t-15">
+            <button type="submit" class="btn btn--radius-2 btn--blue" name="validate">Modify Post</button>
+            </div>
+            </div>
+            </div>
+            </div>
+            
             </form>
         <?php
         }
